5396dee5141183aa6741265287ef50b7cbff3dd3
小周 <slps970093@gmail.com>
2023-05-27 02:06:07 +0800

    [schema] 表結構異動調整
    增加部分欄位防刪除/不顯示出來的欄位

--- src/Storage/migrations/2021_09_18_043426_create_strongadmin_table.php
+++ src/Storage/migrations/2021_09_18_043426_create_strongadmin_table.php
@@ -183,29 +183,30 @@ class CreateStrongadminTable extends Migration
         //strongstrongadmin_menu
         $table_prefix = config("database.connections.{$this->connection}.prefix");
         \DB::connection($this->connection)->unprepared("
-INSERT INTO `{$table_prefix}strongadmin_menu` VALUES (1, 1, 0, '許可權管理', NULL, 1, 2001, 0, '2021-01-06 03:37:40', '2021-05-21 20:10:57', 2);
-INSERT INTO `{$table_prefix}strongadmin_menu` VALUES (2, 2, 1, '菜單管理', 'strongadmin/adminMenu/index', 1, 200, 0, '2021-01-06 03:38:18', '2021-09-18 03:06:27', 2);
-INSERT INTO `{$table_prefix}strongadmin_menu` VALUES (3, 3, 2, '列表檢視', 'strongadmin/adminMenu/index', 1, 200, 0, '2021-01-06 04:50:41', '2021-09-18 03:06:27', 2);
-INSERT INTO `{$table_prefix}strongadmin_menu` VALUES (4, 3, 2, '新增', 'strongadmin/adminMenu/create', 1, 200, 0, '2021-01-06 04:51:07', '2021-09-18 03:06:27', 2);
-INSERT INTO `{$table_prefix}strongadmin_menu` VALUES (5, 3, 2, '更新', 'strongadmin/adminMenu/update', 1, 200, 0, '2021-01-06 04:51:24', '2021-09-18 03:06:27', 2);
-INSERT INTO `{$table_prefix}strongadmin_menu` VALUES (6, 3, 2, '刪除', 'strongadmin/adminMenu/destroy', 1, 200, 0, '2021-01-06 04:51:52', '2021-09-18 03:06:27', 2);
-INSERT INTO `{$table_prefix}strongadmin_menu` VALUES (14, 2, 1, '賬號管理', 'strongadmin/adminUser/index', 1, 200, 0, '2021-01-06 05:21:14', '2021-09-18 03:06:27', 1);
-INSERT INTO `{$table_prefix}strongadmin_menu` VALUES (15, 3, 14, '檢視', 'strongadmin/adminUser/index', 1, 200, 0, '2021-01-06 05:22:05', '2021-09-18 03:06:27', 1);
-INSERT INTO `{$table_prefix}strongadmin_menu` VALUES (16, 3, 14, '更新', 'strongadmin/adminUser/update', 1, 200, 0, '2021-01-06 05:22:45', '2021-09-18 03:06:27', 1);
-INSERT INTO `{$table_prefix}strongadmin_menu` VALUES (17, 3, 14, '刪除', 'strongadmin/adminUser/destroy', 1, 200, 0, '2021-01-06 07:08:39', '2021-09-18 03:06:27', 1);
-INSERT INTO `{$table_prefix}strongadmin_menu` VALUES (18, 2, 1, '角色管理', 'strongadmin/adminRole/index', 1, 200, 0, '2021-01-06 07:09:58', '2021-09-18 03:06:27', 1);
-INSERT INTO `{$table_prefix}strongadmin_menu` VALUES (19, 3, 18, '檢視', 'strongadmin/adminRole/index', 1, 200, 0, '2021-01-06 07:10:18', '2021-09-18 03:06:27', 1);
-INSERT INTO `{$table_prefix}strongadmin_menu` VALUES (20, 3, 18, '新增', 'strongadmin/adminRole/create', 1, 200, 0, '2021-01-06 07:10:30', '2021-09-18 03:06:27', 1);
-INSERT INTO `{$table_prefix}strongadmin_menu` VALUES (21, 3, 18, '更新', 'strongadmin/adminRole/update', 1, 200, 0, '2021-01-06 07:10:48', '2021-09-18 03:06:27', 1);
-INSERT INTO `{$table_prefix}strongadmin_menu` VALUES (22, 3, 18, '刪除', 'strongadmin/adminRole/destroy', 1, 200, 0, '2021-01-06 07:11:02', '2021-09-18 03:06:27', 1);
-INSERT INTO `{$table_prefix}strongadmin_menu` VALUES (23, 2, 1, '操作日誌', 'strongadmin/adminLog/index', 1, 200, 0, '2021-01-07 13:40:48', '2021-09-18 03:06:27', 2);
-INSERT INTO `{$table_prefix}strongadmin_menu` VALUES (24, 3, 23, '刪除', 'strongadmin/adminLog/destroy', 1, 200, 0, '2021-01-07 13:41:44', '2021-09-18 03:06:27', 2);
-INSERT INTO `{$table_prefix}strongadmin_menu` VALUES (25, 3, 23, '檢視', 'strongadmin/adminLog/index', 1, 200, 0, '2021-01-08 02:27:07', '2021-09-18 03:06:27', 2);
-INSERT INTO `{$table_prefix}strongadmin_menu` VALUES (26, 3, 18, '分配許可權', 'strongadmin/adminRole/assignPermissions', 1, 200, 0, '2021-01-08 13:08:33', '2021-09-18 03:06:27', 1);
-INSERT INTO `{$table_prefix}strongadmin_menu` VALUES (36, 1, 0, '主頁', NULL, 1, 99999, 0,'2021-01-08 14:59:46', '2021-01-09 13:58:51', 2);
-INSERT INTO `{$table_prefix}strongadmin_menu` VALUES (37, 2, 36, '面板', 'strongadmin/index/panel', 1, 200, 0, '2021-01-08 16:38:33', '2021-09-18 03:06:27', 2);
-INSERT INTO `{$table_prefix}strongadmin_menu` VALUES (38, 3, 14, '建立', 'strongadmin/adminUser/create', 1, 200, 0, '2021-01-13 15:25:40', '2021-09-18 03:06:27', 1);
-INSERT INTO `{$table_prefix}strongadmin_menu` VALUES (41, 2, 1, 'Telescope', 'strongadmin/telescope/requests', 1, 200, 0, '2021-01-19 17:19:57', '2021-09-18 03:06:27', 1);");
+        INSERT INTO `strongadmin_menu` (`id`, `level`, `parent_id`, `name`, `route_url`, `status`, `sort`, `is_hidden_list`, `created_at`, `updated_at`, `delete_allow`) VALUES (1, 1, 0, '許可權管理', NULL, 1, 2001, 0, '2021-01-06 03:37:40', '2021-05-21 20:10:57', 2);
+INSERT INTO `{$table_prefix}strongadmin_menu` (`id`, `level`, `parent_id`, `name`, `route_url`, `status`, `sort`, `is_hidden_list`, `created_at`, `updated_at`, `delete_allow`) VALUES (2, 2, 1, '菜單管理', 'strongadmin/adminMenu/index', 1, 200, 0, '2021-01-06 03:38:18', '2021-09-18 03:06:27', 2);
+INSERT INTO `{$table_prefix}strongadmin_menu` (`id`, `level`, `parent_id`, `name`, `route_url`, `status`, `sort`, `is_hidden_list`, `created_at`, `updated_at`, `delete_allow`) VALUES (3, 3, 2, '列表檢視', 'strongadmin/adminMenu/index', 1, 200, 0, '2021-01-06 04:50:41', '2021-09-18 03:06:27', 2);
+INSERT INTO `{$table_prefix}strongadmin_menu` (`id`, `level`, `parent_id`, `name`, `route_url`, `status`, `sort`, `is_hidden_list`, `created_at`, `updated_at`, `delete_allow`) VALUES (4, 3, 2, '新增', 'strongadmin/adminMenu/create', 1, 200, 0, '2021-01-06 04:51:07', '2021-09-18 03:06:27', 2);
+INSERT INTO `{$table_prefix}strongadmin_menu` (`id`, `level`, `parent_id`, `name`, `route_url`, `status`, `sort`, `is_hidden_list`, `created_at`, `updated_at`, `delete_allow`) VALUES (5, 3, 2, '更新', 'strongadmin/adminMenu/update', 1, 200, 0, '2021-01-06 04:51:24', '2021-09-18 03:06:27', 2);
+INSERT INTO `{$table_prefix}strongadmin_menu` (`id`, `level`, `parent_id`, `name`, `route_url`, `status`, `sort`, `is_hidden_list`, `created_at`, `updated_at`, `delete_allow`) VALUES (6, 3, 2, '刪除', 'strongadmin/adminMenu/destroy', 1, 200, 0, '2021-01-06 04:51:52', '2021-09-18 03:06:27', 2);
+INSERT INTO `{$table_prefix}strongadmin_menu` (`id`, `level`, `parent_id`, `name`, `route_url`, `status`, `sort`, `is_hidden_list`, `created_at`, `updated_at`, `delete_allow`) VALUES (14, 2, 1, '賬號管理', 'strongadmin/adminUser/index', 1, 200, 0, '2021-01-06 05:21:14', '2021-09-18 03:06:27', 1);
+INSERT INTO `{$table_prefix}strongadmin_menu` (`id`, `level`, `parent_id`, `name`, `route_url`, `status`, `sort`, `is_hidden_list`, `created_at`, `updated_at`, `delete_allow`) VALUES (15, 3, 14, '檢視', 'strongadmin/adminUser/index', 1, 200, 0, '2021-01-06 05:22:05', '2021-09-18 03:06:27', 1);
+INSERT INTO `{$table_prefix}strongadmin_menu` (`id`, `level`, `parent_id`, `name`, `route_url`, `status`, `sort`, `is_hidden_list`, `created_at`, `updated_at`, `delete_allow`) VALUES (16, 3, 14, '更新', 'strongadmin/adminUser/update', 1, 200, 0, '2021-01-06 05:22:45', '2021-09-18 03:06:27', 1);
+INSERT INTO `{$table_prefix}strongadmin_menu` (`id`, `level`, `parent_id`, `name`, `route_url`, `status`, `sort`, `is_hidden_list`, `created_at`, `updated_at`, `delete_allow`) VALUES (17, 3, 14, '刪除', 'strongadmin/adminUser/destroy', 1, 200, 0, '2021-01-06 07:08:39', '2021-09-18 03:06:27', 1);
+INSERT INTO `{$table_prefix}strongadmin_menu` (`id`, `level`, `parent_id`, `name`, `route_url`, `status`, `sort`, `is_hidden_list`, `created_at`, `updated_at`, `delete_allow`) VALUES (18, 2, 1, '角色管理', 'strongadmin/adminRole/index', 1, 200, 0, '2021-01-06 07:09:58', '2021-09-18 03:06:27', 1);
+INSERT INTO `{$table_prefix}strongadmin_menu` (`id`, `level`, `parent_id`, `name`, `route_url`, `status`, `sort`, `is_hidden_list`, `created_at`, `updated_at`, `delete_allow`) VALUES (19, 3, 18, '檢視', 'strongadmin/adminRole/index', 1, 200, 0, '2021-01-06 07:10:18', '2021-09-18 03:06:27', 1);
+INSERT INTO `{$table_prefix}strongadmin_menu` (`id`, `level`, `parent_id`, `name`, `route_url`, `status`, `sort`, `is_hidden_list`, `created_at`, `updated_at`, `delete_allow`) VALUES (20, 3, 18, '新增', 'strongadmin/adminRole/create', 1, 200, 0, '2021-01-06 07:10:30', '2021-09-18 03:06:27', 1);
+INSERT INTO `{$table_prefix}strongadmin_menu` (`id`, `level`, `parent_id`, `name`, `route_url`, `status`, `sort`, `is_hidden_list`, `created_at`, `updated_at`, `delete_allow`) VALUES (21, 3, 18, '更新', 'strongadmin/adminRole/update', 1, 200, 0, '2021-01-06 07:10:48', '2021-09-18 03:06:27', 1);
+INSERT INTO `{$table_prefix}strongadmin_menu` (`id`, `level`, `parent_id`, `name`, `route_url`, `status`, `sort`, `is_hidden_list`, `created_at`, `updated_at`, `delete_allow`) VALUES (22, 3, 18, '刪除', 'strongadmin/adminRole/destroy', 1, 200, 0, '2021-01-06 07:11:02', '2021-09-18 03:06:27', 1);
+INSERT INTO `{$table_prefix}strongadmin_menu` (`id`, `level`, `parent_id`, `name`, `route_url`, `status`, `sort`, `is_hidden_list`, `created_at`, `updated_at`, `delete_allow`) VALUES (23, 2, 1, '操作日誌', 'strongadmin/adminLog/index', 1, 200, 0, '2021-01-07 13:40:48', '2021-09-18 03:06:27', 2);
+INSERT INTO `{$table_prefix}strongadmin_menu` (`id`, `level`, `parent_id`, `name`, `route_url`, `status`, `sort`, `is_hidden_list`, `created_at`, `updated_at`, `delete_allow`) VALUES (24, 3, 23, '刪除', 'strongadmin/adminLog/destroy', 1, 200, 0, '2021-01-07 13:41:44', '2021-09-18 03:06:27', 2);
+INSERT INTO `{$table_prefix}strongadmin_menu` (`id`, `level`, `parent_id`, `name`, `route_url`, `status`, `sort`, `is_hidden_list`, `created_at`, `updated_at`, `delete_allow`) VALUES (25, 3, 23, '檢視', 'strongadmin/adminLog/index', 1, 200, 0, '2021-01-08 02:27:07', '2021-09-18 03:06:27', 2);
+INSERT INTO `{$table_prefix}strongadmin_menu` (`id`, `level`, `parent_id`, `name`, `route_url`, `status`, `sort`, `is_hidden_list`, `created_at`, `updated_at`, `delete_allow`) VALUES (26, 3, 18, '分配許可權', 'strongadmin/adminRole/assignPermissions', 1, 200, 0, '2021-01-08 13:08:33', '2021-09-18 03:06:27', 1);
+INSERT INTO `{$table_prefix}strongadmin_menu` (`id`, `level`, `parent_id`, `name`, `route_url`, `status`, `sort`, `is_hidden_list`, `created_at`, `updated_at`, `delete_allow`) VALUES (36, 1, 0, '主頁', NULL, 1, 99999, 0, '2021-01-08 14:59:46', '2021-01-09 13:58:51', 2);
+INSERT INTO `{$table_prefix}strongadmin_menu` (`id`, `level`, `parent_id`, `name`, `route_url`, `status`, `sort`, `is_hidden_list`, `created_at`, `updated_at`, `delete_allow`) VALUES (37, 2, 36, '面板', 'strongadmin/index/panel', 1, 200, 0, '2021-01-08 16:38:33', '2021-09-18 03:06:27', 2);
+INSERT INTO `{$table_prefix}strongadmin_menu` (`id`, `level`, `parent_id`, `name`, `route_url`, `status`, `sort`, `is_hidden_list`, `created_at`, `updated_at`, `delete_allow`) VALUES (38, 3, 14, '建立', 'strongadmin/adminUser/create', 1, 200, 0, '2021-01-13 15:25:40', '2021-09-18 03:06:27', 1);
+INSERT INTO `{$table_prefix}strongadmin_menu` (`id`, `level`, `parent_id`, `name`, `route_url`, `status`, `sort`, `is_hidden_list`, `created_at`, `updated_at`, `delete_allow`) VALUES (41, 2, 1, 'Telescope', 'strongadmin/telescope/requests', 1, 200, 0, '2021-01-19 17:19:57', '2021-09-18 03:06:27', 1);
+");
     }
 
     /**
